a813ae6bf91d69267114a0420f5f0bfbaa12730f
AD-Codex <avinashdivakaran2000@gmail.com>
2023-11-12 21:12:45 +0530

    new

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/dell/robot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/dell/robot_ws/devel;/home/dell/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/dell/robot_ws/devel;/home/dell/catkin_ws/devel;/opt/ros/noetic' != '' else []
